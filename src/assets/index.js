4d61ee068d97feaf6c21013693ce99eab83d7ec5
Aloyce CodeCraftsman <125455306+Allous-pun@users.noreply.github.com>
2024-07-08 14:58:58 +0300

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,26 @@
+// src/assets/index.js
+
+import profile from './profile.png';
+import avatar1 from './avatar-1.png';
+import avatar2 from './avatar-2.png';
+import avatar3 from './avatar-3.png';
+import avatar4 from './avatar-4.png';
+import webdev from './webdev.svg';
+import webdesign from './webdesign.svg';
+import mobiledev from './mobiledev.svg';
+import aimodelling from './aimodelling.png';
+
+const images = {
+  profile,
+  avatar1,
+  avatar2,
+  avatar3,
+  avatar4,
+  aimodelling,
+  mobiledev,
+  webdesign,
+  webdev
+
+};
+
+export default images;
